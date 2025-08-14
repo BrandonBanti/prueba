1c9eab93c4a0de5cfb3af8cd1f8753d7cced4165
BrandonBanti <brandon.banti.98@gmail.com>
2025-08-13 18:48:16 -0600

    Repositorios JPA

---
+++ src/main/java/com/example/demo/repository/DatailOrdenCompraRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.DetailOrdenCompra;
+
+public interface DatailOrdenCompraRepository extends JpaRepository<DetailOrdenCompra, Long> {
+	
+
+}
